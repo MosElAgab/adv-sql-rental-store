8fe5500cdafd465a08c23fe8f5abc436f7b589dc
MosElAgab <mostafa.elagab@gmail.com>
2023-05-11 20:58:33 +0100

    task 4 completed

--- db/04-alter.sql
+++ db/04-alter.sql
@@ -1,13 +1,23 @@
 \c flix
 
-
+\echo '\n alter original movies table to account for is_sequel\n'
+ALTER TABLE movies 
+ADD is_sequel BOOLEAN
+;
+UPDATE movies
+SET is_sequel =
+CASE WHEN movies.title LIKE '% I %' OR movies.title LIKE '%II%' OR movies.title LIKE '%III%' OR movies.title LIKE '%IV%' OR movies.title LIKE '%IX%'
+THEN TRUE
+ELSE FALSE 
+END
+;
 \echo '\n The store with the highest total of sequels is:'
 -- movises with sequle
 -- stock 
 WITH sequels_stock as (
     SELECT * FROM stock
     WHERE stock.movie_id IN (SELECT movies.movie_id FROM movies
-WHERE movies.title LIKE '% I %' OR movies.title LIKE '%II%' OR movies.title LIKE '%III%' OR movies.title LIKE '%IV%' OR movies.title LIKE '%IX%')
+WHERE movies.is_sequel = TRUE)
 )
 SELECT DISTINCT
 sequels_stock.store_id, stores.city,
